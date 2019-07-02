51dd54475e21bdb318d1bf4386ab78289707c22e
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-07-02 21:54:43 +0300

    backend routes

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
     res.json({ msg: "Weleocme to the Contact Keeper API" });
 });
 
+// define routes
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
