ec7367e259967f17f7b3d99186e6adf75c81824f
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-07-04 00:53:49 +0300

    hash password & register route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
@@ -16,12 +17,36 @@ router.post(
         check("email", "Please include a valid email").isEmail(),
         check("password", "Please enter a password with 6 or more charactes").isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.status(400).json({ errors: errors.array() });
         }
-        res.send("passed");
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            if (user) {
+                res.status(400).json({ msg: "User already exists" });
+            }
+
+            user = new User({
+                name,
+                email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            res.send("user saved");
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        }
     }
 );
 
