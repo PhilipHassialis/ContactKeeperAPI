b59155b1e3f15c4253c5be9de0005a9906172560
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-07-08 07:48:18 +0300

    Edit & update contact action

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -55,7 +55,9 @@ const ContactState = props => {
     };
 
     // update contact
-
+    const updateContact = contact => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact });
+    };
     // filter contacts
 
     // clear filter
@@ -68,7 +70,8 @@ const ContactState = props => {
                 addContact,
                 deleteContact,
                 setCurrent,
-                clearCurrent
+                clearCurrent,
+                updateContact
             }}
         >
             {props.children}
