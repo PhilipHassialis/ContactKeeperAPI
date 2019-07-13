a2c5595b9c4ebe9c023231e22661db760939e996
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-07-13 09:57:12 +0300

    delete contacts

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -21,7 +21,7 @@ export default (state, action) => {
                 loading: false
             };
         case ADD_CONTACT:
-            return { ...state, contacts: [...state.contacts, action.payload], loading: false };
+            return { ...state, contacts: [action.payload, ...state.contacts], loading: false };
         case UPDATE_CONTACT:
             return {
                 ...state,
@@ -29,7 +29,7 @@ export default (state, action) => {
                 loading: false
             };
         case DELETE_CONTACT:
-            return { ...state, contacts: state.contacts.filter(contact => contact.id !== action.payload), loading: false };
+            return { ...state, contacts: state.contacts.filter(contact => contact._id !== action.payload), loading: false };
         case CLEAR_CONTACTS:
             return { ...state, contacts: null, loading: false, error: null, filtered: null, current: null };
         case SET_CURRENT:
